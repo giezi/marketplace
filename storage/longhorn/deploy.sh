49036aa5e0a7b09735d3d21820adbdd43611dda6
giezi <reto@giezi.com>
2020-07-03 15:32:34 +0200

    update

--- storage/longhorn/deploy.sh
+++ storage/longhorn/deploy.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 echo "Deploy Longhorn..."
 sudo kubectl apply -f https://raw.githubusercontent.com/giezi/marketplace/master/storage/longhorn/longhorn-v1.yaml
+
+echo "Deploy loadbalancer..."
+sudo kubectl apply -f https://raw.githubusercontent.com/giezi/marketplace/master/storage/longhorn/loadbalancer.yaml
