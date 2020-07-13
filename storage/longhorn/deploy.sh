09ba5aa9de1320724f9c7aa67b4d9687e6aed9ad
giezi <reto@giezi.com>
2020-07-13 16:36:20 +0200

    longhorn

--- storage/longhorn/deploy.sh
+++ storage/longhorn/deploy.sh
@@ -2,5 +2,9 @@
 echo "Deploy Longhorn..."
 sudo kubectl apply -f https://raw.githubusercontent.com/giezi/marketplace/master/storage/longhorn/longhorn-v1.yaml
 
+# change default storage class
+kubectl patch storageclass longhorn -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+
 echo "Deploy loadbalancer..."
 sudo kubectl apply -f https://raw.githubusercontent.com/giezi/marketplace/master/storage/longhorn/loadbalancer.yaml
